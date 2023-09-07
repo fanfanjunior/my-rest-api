70168d3fd9617869439ce8ae186e807a743b92bd
Delahoya <cihuahuabondet@gmail.com>
2023-09-07 16:43:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Mr.Ic4n'// yourname
+global.MONGO_DB_URI = "mongodb+srv://FannnBOT:2vsWTikUt2Ahk06V@cluster0.bge8hy9.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "irfanchasper112@gmail.com" //email
+global.email_password = "urpellyvpuhlsxbn" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
